4c002783afad099bcd6083c7175a1b7b29128494
Firoze Rakib <frakib@mobiquityinc.com>
2015-01-23 21:05:36 -0500

    updates

--- app/src/main/java/com/firoze/androidplayground/activities/NotificationsActivity.java
+++ app/src/main/java/com/firoze/androidplayground/activities/NotificationsActivity.java
@@ -1,24 +1,26 @@
-package com.firoze.androidplayground;
+package com.firoze.androidplayground.activities;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firoze.androidplayground.R;
+import com.firoze.baseClasses.BaseActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class NotificationsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notifications);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_notifications, menu);
         return true;
     }
 
@@ -35,5 +37,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public String getTag() {
+        return "NotificationsActivity";
     }
 }
--- app/src/main/java/com/firoze/baseClasses/BaseActivity.java
+++ app/src/main/java/com/firoze/baseClasses/BaseActivity.java
@@ -1,13 +1,13 @@
 package com.firoze.baseClasses;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 
 /**
  * Created by firozerakib on 1/19/15.
  * BaseActivity - boilerplate Activity
  */
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends ActionBarActivity {
     private int version_API;
 
     public BaseActivity() {
